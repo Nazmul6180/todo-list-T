827ad0867f94357e3823c4ac77149912463173ac
Nazmul <nazmul.hasan1.61803@gmail.com>
2022-11-14 22:39:23 +0600

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var button = document.getElementById("enter");
-var input = document.getElementById("userinput").focus();
+var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+
+input.focus();
 
 function inputLength() {
 	return input.value.length;
